00d7c8793f61aff70fa0dfd52a0d13eb50bcb26d
Knut Melvær <knut.melvaer@gmail.com>
2020-09-16 22:36:02 +0200

    wip

--- studio/schemas/documents/ad.js
+++ studio/schemas/documents/ad.js
@@ -1,4 +1,48 @@
+import React from 'react'
 import {MdAnnouncement} from 'react-icons/lib/md'
+
+import {DiffAnnotation} from '@sanity/field/diff'
+
+function ColorDiff ({diff}) {
+  const {fromValue, toValue, action} = diff
+  console.log(diff)
+  // In certain cases the diff component will be used to render the value, even
+  // if there are no changes to the actual _value_. For instance, when an item
+  // has been moved within the array, but the actual value did not change.
+  if (!fromValue) {
+    return <div
+      style={{
+        width: '50%',
+        height: '200px',
+        backgroundColor: toValue?.hex
+      }} />
+  }
+
+  // If we have both a "from" and "to" value, the value changed
+  // "from" and "to" can also be read as "previous" and "next"
+
+  return (
+    <DiffAnnotation diff={diff}>
+      <div style={{
+        display: 'flex'
+      }}>
+        <div
+          style={{
+            minWidth: '45%',
+            height: '200px',
+            backgroundColor: fromValue?.hex
+          }} />
+        <div
+          style={{
+            minWidth: '45%',
+            height: '200px',
+            backgroundColor: toValue?.hex
+          }} />
+
+      </div>
+    </DiffAnnotation>
+  )
+}
 
 export default {
   name: 'ad',
@@ -15,6 +59,11 @@ export default {
       name: 'tagline',
       type: 'text',
       title: 'Tagline'
+    },
+    {
+      name: 'brandColor',
+      type: 'color',
+      diffComponent: ColorDiff
     },
     {
       name: 'illustration',
